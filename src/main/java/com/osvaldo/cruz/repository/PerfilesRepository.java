8d8fe335a490e2a38cb06405381860223f6eca63
Osvaldo Cruz <osvaldocruzdelacruz2001@gmail.com>
2021-07-30 18:02:51 -0500

    probando borrar el git ignore

--- src/main/java/com/osvaldo/cruz/repository/PerfilesRepository.java
+++ src/main/java/com/osvaldo/cruz/repository/PerfilesRepository.java
@@ -1,17 +1,9 @@
 package com.osvaldo.cruz.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.osvaldo.cruz.model.Perfil;
-import com.osvaldo.cruz.model.Usuario;
 
 public interface PerfilesRepository extends JpaRepository<Perfil, Integer> {
-	
-	
-	Perfil findByPerfil(String Perfil);
-	
-	@Query(value="select count(*) from Perfiles", nativeQuery=true)
-	Integer totalEntidades();
 
 }
