b3747c4d0ee36dbf0f600aad065b3514aeb8f008
Osvaldo Cruz <osvaldocruzdelacruz2001@gmail.com>
2021-07-30 17:54:26 -0500

    cambio en el repository de perfil

--- src/main/java/com/osvaldo/cruz/repository/PerfilesRepository.java
+++ src/main/java/com/osvaldo/cruz/repository/PerfilesRepository.java
@@ -1,9 +1,17 @@
 package com.osvaldo.cruz.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.osvaldo.cruz.model.Perfil;
+import com.osvaldo.cruz.model.Usuario;
 
 public interface PerfilesRepository extends JpaRepository<Perfil, Integer> {
+	
+	
+	Perfil findByPerfil(String Perfil);
+	
+	@Query(value="select count(*) from Perfiles", nativeQuery=true)
+	Integer totalEntidades();
 
 }
